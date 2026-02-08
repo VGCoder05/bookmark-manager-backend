e2e28c0ed8d2b97d02a3bc4e0a70453885972257
VGCoding <vgcoding05@gmail.com>
2026-02-08 22:47:17 +0530

    added a little development checker - a concole statement

--- src/server.js
+++ src/server.js
@@ -37,12 +37,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Request logging in development
-if (process.env.NODE_ENV === 'development') {
-  app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    next();
-  });
-}
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
 
 // ============================================
 // ROUTES
